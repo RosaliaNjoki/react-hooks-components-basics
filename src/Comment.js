186bb5496632766206e172df65b00de8fdb13f00
RosaliaNjoki <njosali0@gmail.com>
2024-02-27 16:02:53 +0300

    followed the lab

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+  return <p>I do not agree with this article.</p>;
 }
 
 export default Comment;
